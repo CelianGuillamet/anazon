c90adea676ce817a14d38800494f8ed8c683c715
CelianGuillamet <celian.guillamet@gmail.com>
2023-12-21 16:42:22 +0100

    create categories form for new and edit

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,6 +26,40 @@ class CategoryController extends AbstractController
     {
         return $this->render('category/show.html.twig', [
             'category' => $category,
+        ]);
+    }
+
+    #[Route('categories/new', name: 'app_categories_new')]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(CategoryType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($form->getData());
+            $em->flush();
+
+            return $this->redirectToRoute('app_categories');
+        }
+        return $this->render('category/new.html.twig', [
+            'form' => $form
+        ]);
+    }
+
+    #[Route('/categories/{id<^\d+$>}/edit', name: 'app_categories_edit')]
+    public function edit(Category $category, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+
+            return $this->redirectToRoute('app_categories');
+        }
+
+        return $this->render('category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form
         ]);
     }
 }
